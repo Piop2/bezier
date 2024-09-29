7e94086e1c308bd157e2e3795aa523d2bad3b736
piop2 <pioooop2@gmail.com>
2024-09-29 15:05:05 +0900

    setup conan

---
+++ include/bezier.hpp
@@ -0,0 +1,13 @@
+#if !defined(BEZIER_BEZIER_H_)
+#define BEZIER_BEZIER_H_
+
+#ifdef _WIN32
+  #define BEZIER_EXPORT __declspec(dllexport)
+#else
+  #define BEZIER_EXPORT
+#endif
+
+// BEZIER_EXPORT void bezier();
+
+
+#endif // BEZIER_BEZIER_H_
