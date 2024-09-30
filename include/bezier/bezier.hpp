dba00a8f62bac23e24061b1f711cc0516762fe86
piop2 <pioooop2@gmail.com>
2024-10-01 02:15:16 +0900

    add export macro

--- include/bezier/bezier.hpp
+++ include/bezier/bezier.hpp
@@ -1,10 +1,12 @@
 #if !defined(BEZIER_BEZIER_H_)
 #define BEZIER_BEZIER_H_
 
-#ifdef _WIN32
-    #define BEZIER_EXPORT __declspec(dllexport)
-#else
-    #define BEZIER_EXPORT
+#ifndef BEZIER_EXPORT
+    #ifdef _WIN32
+        #define BEZIER_EXPORT __declspec(dllexport)
+    #else
+        #define BEZIER_EXPORT
+    #endif
 #endif
 
 namespace bezier
--- include/bezier/node.hpp
+++ include/bezier/node.hpp
@@ -1,9 +1,17 @@
 #if !defined(BEZIER_NODE_H_)
 #define BEZIER_NODE_H_
 
+#ifndef BEZIER_EXPORT
+    #ifdef _WIN32
+        #define BEZIER_EXPORT __declspec(dllexport)
+    #else
+        #define BEZIER_EXPORT
+    #endif
+#endif
+
 namespace bezier
 {
-    struct Node {
+    struct BEZIER_EXPORT Node {
         float x, y;
 
         Node operator+(const Node& other) const;
