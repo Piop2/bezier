38c09d6ef41730fe171ba3a451b501bc1b2893f9
piop2 <pioooop2@gmail.com>
2024-10-01 03:00:08 +0900

    NDCurve test

--- test_package/src/example.cpp
+++ test_package/src/example.cpp
@@ -1,7 +1,20 @@
+#include <iostream>
+#include <vector>
+
 #include <bezier/curve.hpp>
 
 int main() {
+    std::vector<bezier::Node> nodes = {{0, 0}, {0, 1}, {1, 1}};
 
-    bezier::curve();
+    bezier::NDCurve curve(nodes);
+
+    for (float i = 0.0; i <= 1.1; i += 0.1) {
+        bezier::Node result = curve.Evaluate(i);
+
+        std::cout << "t: " << i;
+        std::cout << ", x: " << result.x;
+        std::cout << ", y: " << result.y << std::endl;
+    }
+
     return 0;
 }
