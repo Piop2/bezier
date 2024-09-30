db05e68900c9b81f8568d89b8c5139ec1383312e
piop2 <pioooop2@gmail.com>
2024-10-01 02:59:43 +0900

    add NDCurve class

--- include/bezier/curve.hpp
+++ include/bezier/curve.hpp
@@ -1,9 +1,31 @@
 #if !defined(BEZIER_CURVE_H_)
 #define BEZIER_CURVE_H_
 
-namespace bezier
-{
-    void curve();
+#include <vector>
+
+#include "bezier/node.hpp"
+
+#ifndef BEZIER_EXPORT
+    #ifdef _WIN32
+        #define BEZIER_EXPORT __declspec(dllexport)
+    #else
+        #define BEZIER_EXPORT
+    #endif
+#endif
+
+namespace bezier {
+    class BEZIER_EXPORT NDCurve
+    {
+    private:
+        std::vector<Node> nodes;
+    public:
+        NDCurve(std::vector<Node> nodes);
+        ~NDCurve();
+        std::vector<Node> GetNodes();
+        void SetNode(std::vector<Node> nodes);
+        Node Evaluate(float t);
+    };
+    
 } // namespace bezier
 
 
--- src/curve.cpp
+++ src/curve.cpp
@@ -2,9 +2,38 @@
 
 #include <iostream>
 
-namespace bezier
-{
-    void curve() {
-        std::cout << "curve!!" << std::endl;
+namespace bezier {
+    NDCurve::NDCurve(std::vector<Node> nodes) {
+        this->nodes = nodes;
+    }
+
+    NDCurve::~NDCurve() {}
+
+    std::vector<Node> NDCurve::GetNodes() {
+        return this->nodes;
+    }
+    
+    void NDCurve::SetNode(std::vector<Node> nodes) {
+        this->nodes = nodes;
+    }
+
+    Node NDCurve::Evaluate(float t) {
+        std::vector<Node> beta = this->nodes;
+        size_t length = beta.size();
+
+        if (length == 0) {
+            // err
+        }
+        if (length == 1) {
+            return beta[0];
+        }
+
+        for (int i = 1; i < length; i++) {
+            for (int j = 0; j < (length - j); j++) {
+                beta[j] = beta[j] * (1 - t) + beta[j + 1] * t;
+            }
+        }
+
+        return beta[0];
     }
 } // namespace bezier
